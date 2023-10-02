bdd72172f6d763a695bd2970aada72e7beacfb8d
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-10-02 19:25:19 -0300

    add Person class

---
+++ schoolLibrary.rb
@@ -0,0 +1,23 @@
+class Person
+  attr_reader :id
+  attr_accessor  :age ,:name
+  def initialize(age, name = "unknown", parent_permission = true)
+    @id = rand(1..1000)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    puts of_age? || @parent_permission
+  end
+
+  private
+  def of_age?
+    @age >= 18
+  end
+end
+
+person_1 = Person.new(18, "ismael", false)
+
+person_1.can_use_services?
