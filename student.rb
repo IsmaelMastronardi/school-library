97dd2c8f260b9d616581322d5e4f00306eb1b8b1
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-10-04 20:05:29 -0300

    fix linters issues

--- student.rb
+++ student.rb
@@ -6,7 +6,6 @@ class Student < Person
 
   def initialize(classroom, age, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
-    @classroom = classroom
   end
 
   def classroom=(classroom)
